1f38b7e89504207efac0e022de49b32ad06a774f
Angelo Stavrow <contact@angelostavrow.com>
2020-05-05 07:09:03 -0400

    Initial Commit

---
+++ DangerTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DangerTest
+//
+//  Created by Angelo Stavrow on 2020-05-05.
+//  Copyright © 2020 Self. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
